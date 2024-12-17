dbbe4bda8246f5fe6c21188c113a547578dfb957
조미현 <Mihyun.Jo@emotion.co.kr>
2024-12-17 11:32:45 +0900

    파일 정리

--- nomad_corder/#4강 ~/js/background.js
+++
@@ -1,6 +0,0 @@
-const images = [ "1.jpg", "2.jpg", "3.jpg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgimg = document.createElement("img");
-
-bgimg.src = `img/${chosenImage}`;
-document.body.appendChild(bgimg);
